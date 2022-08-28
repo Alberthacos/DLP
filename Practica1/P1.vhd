dd669f6a5cb34dc69ff3b7d3a377cb54456eff01
Alberthacos <albertone0127@gmail.com>
2022-08-27 19:31:05 -0500

    propuesta uno de las funciones 4,5,6,

--- Practica1/P1.vhd
+++ Practica1/P1.vhd
@@ -1,26 +1,27 @@
--- Declaración de las funciones de la tabla 1.1
--- Autor: Juan Antonio Jaramillo Gómez
-library ieee; -- Declaración de la biblioteca
-use ieee.std_logic_1164.all;
-entity funciones is -- Declaración de la Entidad
-port ( a,b,c: in std_logic; -- Declaración de los puertos de entrada
-s1,s2,s3: out std_logic -- puertos de salidas
--- s4,s5,s6: OUT std_logic -- salidas propuestas por el equipo
-);
-end funciones; -- Fin de la entidad
--- La asignación de los pines se realiza con el programa PlanAhead o
+-- Declaracion de las funciones de la tabla 1.1
+-- Autor: Juan Antonio Jaramillo Gomez
+LIBRARY ieee; -- Declaracion de la biblioteca
+USE ieee.std_logic_1164.ALL;
+ENTITY funciones IS -- Declaracion de la Entidad
+    PORT (
+        a, b, c : IN STD_LOGIC; -- Declaracion de los puertos de entrada
+        s1, s2, s3, s4, s5, s6 : OUT STD_LOGIC -- puertos de salidas
+        --s4, s5, s6 : OUT std_logic -- salidas propuestas por el equipo
+    );
+END funciones; -- Fin de la entidad
+-- La asignacion de los pines se realiza con el programa PlanAhead o
 -- con un archivo de restricciones de usuario *.ucf, con el siguiente formato:
 -- net "nombre_pin" loc = "pin_FPGA" ;#comentario
-Architecture a_func OF funciones IS --Declaración de la Arquitectura
-begin
--- process (a,b,c) --Inicia el proceso
--- begin
+ARCHITECTURE a_func OF funciones IS --Declaracion de la Arquitectura
+BEGIN
+    -- process (a,b,c) --Inicia el proceso
+    -- begin
 
-S1 <= a xor b xor c; --xor
-S2 <= a or b or c; --or
-S3 <= a xnor b; --xnor
--- S4 <= ...; --asignar otras funciones
--- S5 <= ...; --
--- S6 <= ...; --
--- end process; --Fin del proceso
-end a_func; --Fin de la Arquitectura
+    S1 <= a XOR b XOR c; --xor
+    S2 <= a OR b OR c; --or
+    S3 <= a XNOR b; --xnor
+    S4 <= b NOR c; --asignar otras funciones
+    S5 <= a NAND c; -- NOR B
+    S6 <= a AND b; --
+    -- end process; --Fin del proceso
+END a_func; --Fin de la Arquitectura
